d0730b49851059d673ecc58534b4b303f1e588ab
David PL <david.2289@icloud.com>
2021-05-23 12:42:28 +0200

    Detail Screen

--- app/src/main/java/com/davidpl/brastlewark/ui/adapter/UsersAdapter.kt
+++ app/src/main/java/com/davidpl/brastlewark/ui/adapter/UsersAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.display.ui.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,7 +13,10 @@ import com.squareup.picasso.Picasso
 
 class UsersAdapter(var userList: List<User>, val onItemClick: (user: User) -> Unit) : RecyclerView.Adapter<UsersAdapter.UserVH>() {
 
+    lateinit var context: Context
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserVH {
+        context = parent.context
         val inflater = LayoutInflater.from(parent.context)
         val rootView = inflater.inflate(R.layout.item_user, parent, false)
         return UserVH(rootView)
@@ -21,14 +25,17 @@ class UsersAdapter(var userList: List<User>, val onItemClick: (user: User) -> Un
     override fun onBindViewHolder(holder: UserVH, position: Int) {
         val user = userList.get(position)
         Picasso.get().load(user.thumbnail).into(holder.photo)
-        val completeName = user.name + " " + user.age
-        holder.name.text = completeName
+        holder.name.text = user.name
+        holder.age.text = String.format(context.resources.getString(R.string.screen_list_item_age), user.age.toString())
+        holder.hair.text = String.format(context.resources.getString(R.string.screen_list_item_hair), user.hairColor.toString())
         holder.view.setOnClickListener { onItemClick(user) }
     }
 
     class UserVH(val view: View): RecyclerView.ViewHolder(view) {
         val photo: ImageView = view.findViewById(R.id.photo)
         val name: TextView = view.findViewById(R.id.name)
+        val age: TextView = view.findViewById(R.id.age)
+        val hair: TextView = view.findViewById(R.id.hair)
     }
 
     override fun getItemCount(): Int {
---
+++ app/src/main/java/com/davidpl/brastlewark/ui/fragment/DetailFragment.kt
@@ -0,0 +1,32 @@
+package com.davidpl.brastlewark.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.davidpl.brastlewark.R
+import com.davidpl.brastlewark.business.model.User
+import com.davidpl.brastlewark.databinding.DetailFragmentBinding
+import com.davidpl.brastlewark.ui.utility.helper.Constants
+import com.squareup.picasso.Picasso
+
+class DetailFragment: Fragment() {
+
+    lateinit var binding: DetailFragmentBinding
+    lateinit var user: User
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.detail_fragment, container, false)
+        user = arguments?.getParcelable<User>(Constants.BUNDLE_USER) as User
+        binding.user = user
+        Picasso.get().load(user.thumbnail).into(binding.photo)
+        return binding.root
+    }
+
+}
